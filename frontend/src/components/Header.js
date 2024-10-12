13f12d829f1d171b4fb6ada246d9c702f316886c
khanhdo05 <khanhphuongdo28@gmail.com>
2024-10-12 15:35:56 -0500

    change to profile

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -14,7 +14,7 @@ const Header = () => {
           </li>
           <li className="nav-item">
             <Link to="/" className="nav-link">
-              Bio
+              Profile
             </Link>
           </li>
         </ul>
