e013d1bf095b7638c0f244c75aee3d0b7c204598
Mikhail <mkornilov07@gmail.com>
2024-10-12 13:03:50 -0400

    backend mostly done

--- frontend/src/util/GeoService.js
+++ frontend/src/util/GeoService.js
@@ -2,21 +2,23 @@ import axios from 'axios';
 import React, { useEffect } from 'react';
 
 //insert if location is on
-function GeoService(username, label, setNearbyUsers) {
+function GeoService(userid, username, labelSelector, labelValue, setNearbyUsers) {
     const updateNearbyDelaySec = 5;
     const uploadLocationDelaySec = 1;
     window.navigator.geolocation.getCurrentPosition(console.log)
-    useEffect(()=> {axios.post("/api/user/updateLabel", {"label" : label})}, [label]) //update label in db when label is updated on frontend
+    useEffect(()=> {axios.post("/api/user/updateLabel", {"labelSelector" : labelSelector, "uid":userid})}, [labelSelector]) //update label in db when label is updated on frontend
     useEffect(()=> {
         setInterval(async ()=>{
             window.navigator.geolocation.getCurrentPosition(async (loc) => {
-                let nearbyUsers = await axios.post("api/user/getNearbyUsers", {"latitude" : loc.coords.latitude, "longitude":loc.coords.longitude}) //todo
-                setNearbyUsers(nearbyUsers.filter(t=> t[0] != username))
+                let nearbyUsers = await axios.post("api/user/getNearbyUsers", {"latitude" : loc.coords.latitude, "longitude":loc.coords.longitude, 
+                    "labelSelector" : labelSelector, timestamp : loc.timestamp
+                })
+                setNearbyUsers(nearbyUsers.filter(t=> t[0] != userid))
             })
         }, updateNearbyDelaySec * 1000);
         setInterval(async () => {
             window.navigator.geolocation.getCurrentPosition((loc) => {
-                axios.post("/api/user/updateLoc", {"latitude" : loc.coords.latitude, "longitude": loc.coords.longitude, "timestamp" : loc.timestamp, "username" : username})
+                axios.post("/api/user/updateLoc", {"latitude" : loc.coords.latitude, "longitude": loc.coords.longitude, "timestamp" : loc.timestamp, "uid" : userid})
             })
         }, uploadLocationDelaySec * 1000)},
     [])
