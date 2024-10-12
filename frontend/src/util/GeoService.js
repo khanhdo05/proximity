666464c9e4f1bbda199da0b362e252932245ef57
Mikhail <mkornilov07@gmail.com>
2024-10-12 17:59:00 -0400

    unnecessary prints

--- frontend/src/util/GeoService.js
+++ frontend/src/util/GeoService.js
@@ -15,7 +15,7 @@ function GeoService(userid, labelSelector, setNearbyUsers) {
     window.navigator.geolocation.getCurrentPosition(console.log)
     useEffect(()=> {axios.post("http://localhost:8080/api/user/updateLabel", {"labelSelector" : labelSelector, "uid":userid})}, [labelSelector]) //update label in db when label is updated on frontend
     async function handleLoc(loc) {
-        console.log("about to send location")
+        // console.log("about to send location")
         let nearbyUsers
         try {
         nearbyUsers = (await axios.post("http://localhost:8080/api/user/getNearbyUsers", {"latitude" : loc.coords.latitude, "longitude":loc.coords.longitude, 
@@ -24,13 +24,13 @@ function GeoService(userid, labelSelector, setNearbyUsers) {
         catch (e) {
             console.log(e)
         }
-            console.log("gsdgdg")
-        console.log(`Got ${(nearbyUsers)}`)
+            // console.log("gsdgdg")
+        // console.log(`Got ${(nearbyUsers)}`)
             setNearbyUsers(nearbyUsers)//.filter(t => t[0] !== userid))
             await axios.post("http://localhost:8080/api/user/updateLoc", {"latitude" : loc.coords.latitude, "longitude": loc.coords.longitude, "timestamp" : loc.timestamp, "uid" : userid})
             // await new Promise(r => setTimeout(r, checkLocDelaySec * 1000));
         }
-    setInterval(()=> {console.log("starting location"); window.navigator.geolocation.getCurrentPosition(handleLoc)}, checkLocDelaySec * 1000)
+    setInterval(()=> {window.navigator.geolocation.getCurrentPosition(handleLoc)}, checkLocDelaySec * 1000)
     // useEffect(()=>{window.navigator.geolocation.watchPosition(handleLoc, ()=>{})})
     
     return <></>
