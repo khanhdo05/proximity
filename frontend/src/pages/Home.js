6cfa2fc69a655eadfc6104642395b9c0b59bc6bd
khanhdo05 <khanhphuongdo28@gmail.com>
2024-10-15 11:12:36 -0500

    change name

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,10 +1,12 @@
-import MapComponent from '../components/UserLocationMap';
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import UserLocationMap from '../components/UserLocationMap';
+import Chat from '../components/Chat';
 
 function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <MapComponent />
+      <UserLocationMap />
     </div>
   );
 }
